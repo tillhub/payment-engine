c97337bddcb913855c28503c75d58b1d2d08981d
Đorđe Hrnjez <djordjehrnjez@gmail.com>
2022-12-16 10:15:35 +0100

    Project setup

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    kotlin(Dependencies.KotlinPlugins.ANDROID)
+    kotlin(Dependencies.KotlinPlugins.KAPT)
+    id(Dependencies.Plugins.LIBRARY)
+    id(Dependencies.Plugins.DETEKT) version Versions.Plugins.DETEKT
+    id(Dependencies.Plugins.PUBLISH)
+}
+
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+android {
+    compileSdk = ConfigData.targetSdkVersion
+
+    defaultConfig {
+        minSdk = ConfigData.minSdkVersion
+        targetSdk = ConfigData.targetSdkVersion
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        getByName(ConfigData.BuildType.RELEASE) {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = ConfigData.JAVA_VERSION
+        targetCompatibility = ConfigData.JAVA_VERSION
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = ConfigData.JVM_TARGET
+        }
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+    // Core Dependencies
+    implementDependencyGroup(Dependencies.Groups.CORE)
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    // Utils
+    implementation(Dependencies.Tools.TIMBER)
+
+    // Unit tests
+    implementDependencyGroup(Dependencies.Groups.TEST_LIBRARIES)
+    implementDependencyGroup(Dependencies.Groups.TEST_ROBOLECTRIC)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>(ConfigData.artifactId) {
+                groupId = ConfigData.applicationId
+                artifactId = ConfigData.artifactId
+                version = ConfigData.versionName
+
+                from(components.getByName("release"))
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "Tillhub Payment Engine"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
