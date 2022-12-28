3895f6d06a109f10ae4938263957be10f98a1a3b
Đorđe Hrnjez <djordjehrnjez@gmail.com>
2022-12-28 13:52:42 +0100

    Provide terminal manager

--- src/main/java/de/tillhub/paymentengine/di/PaymentModule.kt
+++ src/main/java/de/tillhub/paymentengine/di/PaymentModule.kt
@@ -3,17 +3,19 @@ package de.tillhub.paymentengine.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import de.tillhub.paymentengine.TerminalManager
 import de.tillhub.paymentengine.TerminalManagerImpl
 import de.tillhub.paymentengine.data.LavegoTransactionDataConverter
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object PaymentModule {
 
     @Provides
-    fun provideCardPaymentManager(
+    @Singleton
+    fun provideTerminalManager(
         paymentTime: PaymentTime,
         lavegoTransactionDataConverter: LavegoTransactionDataConverter,
     ): TerminalManager {
