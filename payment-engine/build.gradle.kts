c7e8d1002b9843fba5bb8251383e7257e5af5478
Đorđe Hrnjez <djordjehrnjez@gmail.com>
2024-07-16 09:52:53 +0200

    Downgrade of lifecycle

--- payment-engine/build.gradle.kts
+++ payment-engine/build.gradle.kts
@@ -92,7 +92,7 @@ afterEvaluate {
             create<MavenPublication>("payment-engine") {
                 groupId = "de.tillhub.paymentengine"
                 artifactId = "payment-engine"
-                version = "2.1.0"
+                version = "2.1.1"
 
                 from(components.getByName("release"))
             }
