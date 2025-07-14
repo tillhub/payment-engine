a9d35adabe7d54d3e21a03e2ec703f3f674c31ba
Martin <m.sirok@gmail.com>
2025-07-14 10:56:57 +0200

    updated to 3.7.0

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -5,7 +5,7 @@ object Configs {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 24
     const val MIN_SDK_SOFT_PAY = 26
-    const val VERSION_CODE = "3.6.5"
+    const val VERSION_CODE = "3.7.0"
     val JAVA_VERSION = JavaVersion.VERSION_17
     val JVM_TARGET = JAVA_VERSION.toString()
 }
